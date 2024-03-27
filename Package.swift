efef8f3fd54fe6db0a18efb9945552cec4ebcf14
Eugene <iosdeveloper.mail@gmail.com>
2024-03-27 17:29:21 +0100

    Revert "Package updated."
    This reverts commit ebcc74785d7873428531e02f7a3f37cff49b6271.

--- Package.swift
+++ Package.swift
@@ -6,21 +6,27 @@ import PackageDescription
 let package = Package(
     name: "DGChatWidgetPackage",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13), // Adjust the platform version as needed
     ],
-    products: [.
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DGChatWidgetPackage",
             targets: ["DGChatWidgetPackage"]),
     ],
-    dependencies: [ ],
+    dependencies: [
+        // Add any dependencies here
+    ],
     targets: [
         .binaryTarget(
             name: "DGChatSDK",
-            path: "DGChatSDK.xcframework"
+            path: "DGChatSDK.xcframework" // Adjust the path to your XCFramework
         ),
         .target(
             name: "DGChatWidgetPackage",
-            dependencies: ["DGChatSDK"]),
+            dependencies: ["DGChatSDK"]), // Add the binary target as a dependency
+        .testTarget(
+            name: "DGChatWidgetPackageTests",
+            dependencies: ["DGChatWidgetPackage"]),
     ]
 )
